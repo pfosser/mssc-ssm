5bcb1057e5a1ff401971beb8d122b8be198db8d6
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-12-12 08:59:26 +0100

    Lesson 207. Logging Configuration Using State Change Listeners

--- src/main/java/it/paofos/msscssm/config/StateMachineFactory.java
+++ src/main/java/it/paofos/msscssm/config/StateMachineFactory.java
@@ -2,12 +2,15 @@ package it.paofos.msscssm.config;
 
 import org.springframework.stereotype.Component;
 
+import com.github.oxo42.stateless4j.StateConfiguration;
 import com.github.oxo42.stateless4j.StateMachine;
 import com.github.oxo42.stateless4j.StateMachineConfig;
 
 import it.paofos.msscssm.domain.PaymentEvent;
 import it.paofos.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class StateMachineFactory {
 
@@ -15,14 +18,20 @@ public class StateMachineFactory {
 
 		StateMachineConfig<PaymentState, PaymentEvent> paymentConfig = new StateMachineConfig<>();
 
-		paymentConfig.configure(PaymentState.NEW) //
+		addTransitionLog(paymentConfig.configure(PaymentState.NEW) //
 				.ignore(PaymentEvent.PRE_AUTHORIZE) //
 				.permit(PaymentEvent.PRE_AUTH_APPROVED, PaymentState.PRE_AUTH) //
-				.permit(PaymentEvent.PRE_AUTH_DECLINED, PaymentState.PRE_AUTH_ERROR);
+				.permit(PaymentEvent.PRE_AUTH_DECLINED, PaymentState.PRE_AUTH_ERROR));
 
-		paymentConfig.configure(PaymentState.PRE_AUTH) //
-				.ignore(PaymentEvent.PRE_AUTH_DECLINED);
+		addTransitionLog(paymentConfig.configure(PaymentState.PRE_AUTH) //
+				.ignore(PaymentEvent.PRE_AUTH_DECLINED));
 
 		return new StateMachine<PaymentState, PaymentEvent>(PaymentState.NEW, paymentConfig);
+	}
+
+	private StateConfiguration<PaymentState, PaymentEvent> addTransitionLog(
+			StateConfiguration<PaymentState, PaymentEvent> config) {
+		return config.onEntry((transition) -> log
+				.info("State changed (from %s to %s)".formatted(transition.getSource(), transition.getDestination()))); //
 	}
 }
